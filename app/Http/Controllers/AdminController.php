71ef0798795814e0f78ba8148201a541dca328a4
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2023-01-26 21:04:33 +0200

    feat: add admin logout option

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function AdminDashboard()
     {
         return view('admin.index');
+    }
+
+    public function AdminLogin()
+    {
+        return view('admin.login');
+    }
+
+    public function AdminDestroy(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
     }
 }
